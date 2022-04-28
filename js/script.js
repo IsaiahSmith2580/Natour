b36504ece4f4466284348978e60b2f8f729f7abe
IsaiahSmith2580 <95199294+IsaiahSmith2580@users.noreply.github.com>
2022-04-28 11:49:00 +0100

    nav update

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.nav-header');
+
+btnNavEl.addEventListener('click', function () {
+  console.log(headerEl.classList.toggle('nav-open'));
+});
+
+console.log(btnNavEl);
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
+//////////////////////////////////////////////////////////
